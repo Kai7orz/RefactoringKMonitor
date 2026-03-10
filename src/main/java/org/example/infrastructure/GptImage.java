8ae39a7e63cfdc8184aff08477b6d4ef94d7e264
Kai7orz <kai7orz@gmail.com>
2026-03-10 23:45:14 +0900

    イラストメソッドのたたき台実装

--- src/main/java/org/example/infrastructure/GptImage.java
+++ src/main/java/org/example/infrastructure/GptImage.java
@@ -1,4 +1,15 @@
 package org.example.infrastructure;
 
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.images.ImageGenerateParams;
+import com.openai.models.images.ImagesResponse;
+import org.springframework.beans.factory.annotation.Value;
+
 public class GptImage {
+    public GptImage(){
+        OpenAIClient openAIClient  = OpenAIOkHttpClient.fromEnv();
+        ImageGenerateParams params = ImageGenerateParams.builder().prompt("Create a illustration based on submitted image").build();
+        ImagesResponse imagesResponse = openAIClient.images().generate(params);
+    }
 }
